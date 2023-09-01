79f9c17cd640530b24423aaf10deea7c24f98811
Asmi Anand <120274463+anandasmi@users.noreply.github.com>
2023-09-01 20:09:59 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,4 @@
+x, y = 100, 1000
+
+result = "x is less than y." if x > y else "y is more than x."
+print(result)
---
+++ takinginputfromuser1.py
@@ -0,0 +1,5 @@
+# Taking input from user
+
+print("Hello?")
+name = input("Your Name?")
+print("Hello,", name)
